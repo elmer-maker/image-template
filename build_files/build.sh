058970408e19854fac3bfde4a805a432f35b5cbc
elmer-maker <vyazankinvlad@gmail.com>
2026-06-02 22:00:40 +0200

    Add Cosmic Desktop + fastfetch + distrobox on top of Bazzite base

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,21 +4,14 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf5 install -y fastfetch distrobox
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Cosmic Desktop Environment (official COPR from Ryan Brue / System76 team)
+dnf5 -y copr enable ryanabx/cosmic-epoch
+dnf5 -y install cosmic-desktop
+dnf5 -y copr disable ryanabx/cosmic-epoch
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+# Steam + excellent hardware support (WiFi, Bluetooth, graphics, audio, etc.)
+# already come from the Bazzite base. Flatpak is also ready.
 
 systemctl enable podman.socket
